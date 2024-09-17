2d35562f54215548d06165df26726e9316ec5729
Pedro Vitor Emanuel <ped.frsouza@gmail.com>
2024-09-17 02:30:10 -0300

    OpenAPIDefinition - CORS fix problem

--- src/main/java/com/dio/space/Application.java
+++ src/main/java/com/dio/space/Application.java
@@ -1,11 +1,14 @@
 package com.dio.space;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @EnableFeignClients
 @SpringBootApplication
+@OpenAPIDefinition(servers = {@Server(url = "/", description = "Default Server URL")})
 public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
